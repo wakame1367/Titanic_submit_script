d96fdf23e4f86caaa534dc1734d33c71d7371b01
wakamezake <hotwater1367@gmail.com>
2019-12-24 22:52:39 +0900

    support windows os

--- build.py
+++ build.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import base64
 import gzip
+import os
 from pathlib import Path
+
+is_Windows = True if os.name == "nt" else False
 
 
 def encode_file(path: Path) -> str:
@@ -11,7 +14,12 @@ def encode_file(path: Path) -> str:
 
 def build_script():
     to_encode = list(Path('titanic_sample').glob('*.py')) + [Path('setup.py')]
-    file_data = {str(path): encode_file(path) for path in to_encode}
+    if is_Windows:
+        # https://stackoverflow.com/questions/54671385/convert-windowspath-to-posixpath
+        file_data = {str(path.as_posix()): encode_file(path) for path in
+                     to_encode}
+    else:
+        file_data = {str(path): encode_file(path) for path in to_encode}
     template = Path('script_template.py').read_text('utf8')
     Path('build/script.py').write_text(
         template.replace('{file_data}', str(file_data)),
